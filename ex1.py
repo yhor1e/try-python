dd1ba4e69b58bfc57857e4792d465837f7213318
yhorie <yuk.horie@gmail.com>
2020-03-02 00:16:42 +0900

    Finish ex1.py

---
+++ ex1.py
@@ -0,0 +1,22 @@
+x = 2
+rnew = x
+
+r1 = rnew
+r2 = x/r1
+rnew = (r1 + r2) / 2
+print(r1, rnew, r2)
+
+r1 = rnew
+r2 = x/r1
+rnew = (r1 + r2) / 2
+print(r1, rnew, r2)
+
+r1 = rnew
+r2 = x/r1
+rnew = (r1 + r2) / 2
+print(r1, rnew, r2)
+
+r1 = rnew
+r2 = x/r1
+rnew = (r1 + r2) / 2
+print(r1, rnew, r2)
